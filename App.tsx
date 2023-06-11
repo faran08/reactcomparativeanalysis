028b4448d40d91a8194e41a6004662eda76d457b
Faran Mushtaq <faran08@live.com>
2023-06-11 19:30:41 +0500

    first commit

---
+++ App.tsx
@@ -0,0 +1,178 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React, { useState } from 'react';
+import type {PropsWithChildren} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+  TouchableOpacity,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+type SectionProps = PropsWithChildren<{
+  title: string;
+}>;
+
+
+
+function App(): JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+  const [input, setInput] = useState('');
+  const [result, setResult] = useState('');
+
+  const handleInput = (value: string) => {
+    if (value === '=') {
+      setResult(eval(input).toString());
+      setInput('');
+    } else if (value === 'C') {
+      setResult('');
+      setInput('');
+    } else {
+      setInput(input + value);
+      setResult(input+ value);
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Calculator App</Text>
+      <View style={styles.resultBox}>
+        <Text style={styles.result}>{result}</Text>
+      </View>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity style={styles.button} onPress={() => handleInput('7')}>
+          <Text style={styles.buttonText}>7</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={() => handleInput('8')}>
+          <Text style={styles.buttonText}>8</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={() => handleInput('9')}>
+          <Text style={styles.buttonText}>9</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={() => handleInput('/')}>
+          <Text style={styles.buttonText}>/</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={() => handleInput('4')}>
+          <Text style={styles.buttonText}>4</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={() => handleInput('5')}>
+          <Text style={styles.buttonText}>5</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={() => handleInput('6')}>
+          <Text style={styles.buttonText}>6</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={() => handleInput('*')}>
+          <Text style={styles.buttonText}>*</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={() => handleInput('1')}>
+          <Text style={styles.buttonText}>1</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={() => handleInput('2')}>
+          <Text style={styles.buttonText}>2</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={() => handleInput('3')}>
+          <Text style={styles.buttonText}>3</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={() => handleInput('-')}>
+          <Text style={styles.buttonText}>-</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={() => handleInput('0')}>
+          <Text style={styles.buttonText}>0</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={() => handleInput('.')}>
+          <Text style={styles.buttonText}>.</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={() => handleInput('=')}>
+          <Text style={styles.buttonText}>=</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={() => handleInput('+')}>
+          <Text style={styles.buttonText}>+</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={[styles.button, { backgroundColor: 'red' }]} onPress={() => handleInput('C')}>
+          <Text style={styles.buttonText}>C</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+
+
+ 
+  )
+
+
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F5F5F5',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 10,
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: 'black',
+    marginBottom: 20,
+  },
+  resultBox: {
+    backgroundColor: 'white',
+    borderRadius: 20,
+    width: '100%',
+    alignItems: 'flex-end',
+    padding: 20,
+    marginBottom: 20,
+    
+  },
+  input: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: Colors.black
+  },
+  result: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: Colors.black
+  },
+  buttonContainer: {
+    width: '100%',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between',
+  },
+  button: {
+    backgroundColor: 'white',
+    borderRadius: 10,
+    width: '23%',
+    aspectRatio: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 10,
+  },
+  buttonText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: Colors.black
+  },
+});
+
+
+
+export default App;
